75ecef0e274748fd68f548b8d3a7fa32f0d2cb50
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-14 19:14:03 -0300

    first commit

---
+++ usuario_backend/src/main/java/com/bhh/usuario_backend/UsuarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bhh.usuario_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioBackendApplication.class, args);
+	}
+
+}
---
+++ usuario_backend/src/test/java/com/bhh/usuario_backend/UsuarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhh.usuario_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
