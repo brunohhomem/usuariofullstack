da81c49cf374e96c6a428dcb45c10a577639fcbc
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-16 16:53:04 -0300

    config angular routes

--- usuario_frontend/src/app/app-routing.module.ts
+++ usuario_frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './Components/user/user.component';
+import { RoleComponent } from './Components/role/role.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "users", component: UserComponent},
+  {path: "roles", component: RoleComponent},
+  {path: '', redirectTo: '/users', pathMatch: 'full'} //redirect to 'UserComponent' if the path is empty
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
